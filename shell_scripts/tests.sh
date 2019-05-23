31e27d288a67ed7f30d4cbffce8d59a746d96749
David Espí Hernández <davidespihernandez@gmail.com>
2019-05-23 08:39:49 +0200

    Update tests.sh

--- shell_scripts/tests.sh
+++ shell_scripts/tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 . ~/.virtualenvs/oracleutils/bin/activate
 cd ../django/oracleutils
-python3 manage.py test module1 --testrunner=oracleutils.NoDbTestRunner --verbosity=0
-python3 manage.py test module2 --testrunner=oracleutils.NoDbTestRunner --verbosity=0
+python3 manage.py test module1 --testrunner=oracleutils.NoDbTestRunner
+python3 manage.py test module2 --testrunner=oracleutils.NoDbTestRunner
