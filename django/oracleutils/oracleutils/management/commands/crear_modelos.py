8e6b9d4ac1458037cb1ce3ed7aa13d8b3eaa21c1
David Espí Hernández <davidespihernandez@gmail.com>
2019-06-25 19:34:37 +0200

    Borrar ficheros de modelos antes de recrear

--- django/oracleutils/oracleutils/management/commands/crear_modelos.py
+++ django/oracleutils/oracleutils/management/commands/crear_modelos.py
@@ -1,4 +1,6 @@
+import os
 from contextlib import redirect_stdout
+from os import listdir
 
 from django.core.management.base import BaseCommand
 from django.core.management.commands.inspectdb import (
@@ -13,10 +15,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for app, tables in ALL_TABLES.items():
-            with open(f'{app}/models/__init__.py', 'w') as init_file:
+            folder = f'{app}/models/'
+            for file_name in listdir(folder):
+                if file_name.endswith('.py'):
+                    os.remove(folder + file_name)
+
+            with open(f'{folder}__init__.py', 'w') as init_file:
                 for table in tables:
                     table_name = table.lower()
-                    with open(f'{app}/models/{table_name}.py', 'w') as f:
+                    with open(f'{folder}{table_name}.py', 'w') as f:
                         with redirect_stdout(f):
                             InspectDbCommand().execute(
                                 table=[table],
